68b3b623dcdf66868f60a212e3608013bc73e868
ngocht2 <ngocht@blockchainlabs.asia>
2023-09-12 02:15:40 +0700

    create, sign and submit txs

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,16 @@ import { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 const projectId = "previewpk7DHYd9OCLdwAF0PuWVygYaN0cDuGy6";
+
 export default function Home() {
   const [lucid, setLucid] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState(null);
+  const [tx, setTx] = useState(null);
 
-  const handleRequestAccess = async () => {
+  const requestAccessTo = async () => {
     try {
+      setLoading(true);
       const _lucid = await Lucid.new(
         new Blockfrost(
           "https://cardano-preview.blockfrost.io/api/v0",
@@ -19,105 +24,207 @@ export default function Home() {
         "Preview"
       );
       const api = await window.cardano.nami.enable();
-      // Assumes you are in a browser environment
       const wallet = _lucid.selectWallet(api);
       setLucid(wallet);
     } catch (error) {
       console.log("handling Request access failed: ", error);
+    } finally {
+      setLoading(false);
     }
   };
+
+  const getBalance = async () => {
+    try {
+      setLoading(true);
+      const utxos = await lucid.wallet.getUtxos();
+      const lovelace = utxos.reduce(
+        (amount, utxo) => amount + utxo.assets.lovelace,
+        0n
+      );
+
+      setResults(lovelace.toString());
+    } catch (error) {
+      console.log("getting account Balance failed: ", error);
+      setResults(error.message || error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getUtxos = async () => {
+    try {
+      setLoading(true);
+      const utxos = await lucid.wallet.getUtxos();
+      setResults(
+        JSON.stringify(
+          utxos.map((item) => {
+            item.assets.lovelace = item.assets.lovelace.toString();
+            return item;
+          })
+        )
+      );
+    } catch (error) {
+      console.log("getting account utxos failed: ", error);
+      setResults(error.message || error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const createTx = async () => {
+    try {
+      setLoading(true);
+      const recipient =
+        "addr_test1qpfnvd95mppgpkp3hpwjwyg3aj2dy0swqy0ztuapqv5l07lry3xhrkkpqcqq0m3qzd9s7q698x5qqwflwjdekdvjrwlqrkqnpw";
+
+      const lovelace = 1000000n;
+      const tx = await lucid
+        .newTx()
+        .payToAddress(recipient, { lovelace })
+        .complete();
+      setTx(tx);
+      setResults("create the tx successfully");
+    } catch (error) {
+      console.log("creating a tx  failed: ", error);
+      setResults(error.message || error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signTx = async () => {
+    try {
+      setLoading(true);
+      const signedTx = await tx.sign().complete();
+      setTx(signedTx);
+      setResults("sign the tx successfully");
+    } catch (error) {
+      setResults(error.message || error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const submitTx = async () => {
+    try {
+      setLoading(true);
+      const txHash = await tx.submit();
+      setResults(txHash);
+    } catch (error) {
+      setResults(error.message || error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1 className="text-5xl text-center my-3">Cardano dApp Example</h1>
-      {!lucid && (
-        <div className="col-12 text-center my-3">
-          <button
-            className="px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm"
-            onClick={handleRequestAccess}
+    <main className={` ${inter.className}`}>
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-5xl text-center my-3">Cardano dApp Example</h1>
+        {!lucid && (
+          <div className="col-12 text-center my-3">
+            <button
+              className="px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm"
+              onClick={requestAccessTo}
+            >
+              Request access to Nami
+            </button>
+          </div>
+        )}
+
+        <div className="grid gap-4 grid-cols-2 my-3">
+          <div className="grid gap-4 grid-cols-1">
+            <button
+              disabled={!lucid}
+              className="px-4 py-2 w-full  text-sm bg-gray-200 rounded-md shadow-sm disabled:opacity-50 disabled:cursor-not-allowed"
+              onClick={getBalance}
+            >
+              Get Account Balance
+            </button>
+            <button
+              disabled={true}
+              className="px-4 py-2 w-full  text-sm bg-gray-200 rounded-md shadow-sm disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Get Used Addresses
+            </button>
+            <button
+              disabled={true}
+              className="px-4 py-2 w-full  text-sm bg-gray-200 rounded-md shadow-sm disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Get Unused Addresses
+            </button>
+            <button
+              className="px-4 py-2 w-full  text-sm bg-gray-200 rounded-md shadow-sm disabled:opacity-50 disabled:cursor-not-allowed"
+              disabled={true}
+            >
+              Get Change Addresses
+            </button>
+          </div>
+          <div className="grid gap-4 grid-cols-1">
+            <button
+              disabled={!lucid}
+              className="px-4 py-2 w-full  text-sm bg-gray-200 rounded-md shadow-sm disabled:opacity-50 disabled:cursor-not-allowed"
+              onClick={getUtxos}
+            >
+              Get Utxos
+            </button>
+            <button
+              disabled={!lucid}
+              className="px-4 py-2 w-full  text-sm bg-gray-200 rounded-md shadow-sm disabled:opacity-50 disabled:cursor-not-allowed"
+              onClick={createTx}
+            >
+              Create Tx
+            </button>
+            <button
+              disabled={!tx}
+              onClick={signTx}
+              className="px-4 py-2 w-full  text-sm bg-gray-200 rounded-md shadow-sm disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Sign Tx
+            </button>
+            <button
+              disabled={!tx}
+              className="px-4 py-2 w-full  text-sm bg-gray-200 rounded-md shadow-sm disabled:opacity-50 disabled:cursor-not-allowed"
+              onClick={submitTx}
+            >
+              Submit Tx
+            </button>
+          </div>
+        </div>
+        <div
+          className={`rounded-lg w-2/3 p-4 break-all my-8  bg-orange-50 border border-orange-300`}
+          style={{ minHeight: 120 }}
+        >
+          {results}
+        </div>
+      </div>
+      {loading && (
+        <div className="w-full h-full absolute top-0 bg-white opacity-50"></div>
+      )}
+      {loading && (
+        <div className="absolute inset-1/2">
+          <svg
+            className="w-10 h-10 animate-spin -ml-1 mr-3 text-indigo-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
           >
-            Request access to Nami
-          </button>
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
         </div>
       )}
-
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
